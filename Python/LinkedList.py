5d7c9fe7f5b93a77e7ff7beede08a548a2a0384a
Vipul Cariappa <vipulcariappa@gmail.com>
2022-12-12 15:28:19 +0530

    added doc string

--- Python/LinkedList.py
+++ Python/LinkedList.py
@@ -3,20 +3,39 @@ class ListNode:
         self.value = value
         self.next = None
 
-    # def __str__(self):
-    #     if self.next is None:
-    #         return f"self: {id(self)} next: None"
+    def __str__(self):
+        if self.next is None:
+            return f"self: {id(self)} next: None"
 
-    #     return f"self: {id(self)} next: {id(self.next)}"
+        return f"self: {id(self)} next: {id(self.next)}"
 
 
 class LinkedList:
-    def __init__(self):
+    """LinkedList
+    """
+    def __init__(self, values=None):
+        """initialise the list with values
+
+        Args:
+            values (Iterable): value to be added to LinkedList
+        """
         self.head = None
         self.tail = None
         self.length = 0
 
+        if values is None:
+            return
+
+        for i in values:
+            self.add_at_end(i)
+
     def add_at_start(self, value):
+        """add value at the start of list
+        time complexity is O(1)
+
+        Args:
+            value (Any): value to be added
+        """
         old_head = self.head
         self.head = ListNode(value)
         self.head.next = old_head
@@ -27,6 +46,12 @@ class LinkedList:
         self.length += 1
 
     def add_at_end(self, value):
+        """add value at the end of the list
+        time complexity is O(n)
+
+        Args:
+            value (Any): value to be added
+        """
         if self.length == 0:
             self.head = ListNode(value)
             self.tail = self.head
@@ -38,6 +63,16 @@ class LinkedList:
         self.length += 1
 
     def add_at(self, index, value):
+        """add value at the specified index
+        time complexity is O(n)
+
+        Args:
+            index (int): index to insert at
+            value (Any): value to be added
+
+        Raises:
+            IndexError: when invalid index or negative index is passed
+        """
         if index < 0 or index >= self.length:
             raise IndexError(
                 f"Index out of Bound. Length of list {self.length}, index got to insert at {index}."
@@ -60,6 +95,15 @@ class LinkedList:
         self.length += 1
 
     def remove_at_start(self):
+        """removes the element at beginning of the list
+        time complexity is O(n)
+
+        Raises:
+            IndexError: when no element is present in the list
+
+        Returns:
+            Any: element at start of the list
+        """
         if self.length == 0:
             raise IndexError("No elements in list to remove.")
 
@@ -73,7 +117,15 @@ class LinkedList:
         return node.value
 
     def remove_at_end(self):
-        # time complexity is O(n)
+        """remove the element at the end of the list
+        time complexity is O(n)
+
+        Raises:
+            IndexError: when no element is present
+
+        Returns:
+            Any: element at end of the list
+        """
         if self.length == 0:
             raise IndexError("No elements in list to remove.")
 
@@ -93,6 +145,18 @@ class LinkedList:
         return node.value
 
     def remove_at(self, index):
+        """remove the element present at the given index
+        time complexity is O(n)
+
+        Args:
+            index (int): index of the element to be removed
+
+        Raises:
+            IndexError: when invalid or negative index is passed
+
+        Returns:
+            Any: element at the given index
+        """
         if index < 0 or index >= self.length:
             raise IndexError(
                 f"Index out of Bound. Length of list {self.length}, index got to insert at {index}."
@@ -160,18 +224,14 @@ class LinkedList:
 
         return result
 
-    # def __repr__(self):
-    #     result = "[\n"
+    def __repr__(self):
+        result = "[\n"
 
-    #     i = self.head
-    #     while i is not None:
-    #         result += "\t" + str(i) + ",\n"
-    #         i = i.next
+        i = self.head
+        while i is not None:
+            result += "\t" + str(i) + ",\n"
+            i = i.next
 
-    #     result += "]"
+        result += "]"
 
-    #     return result
-
-
-if __name__ == "__main__":
-    ...
+        return result
